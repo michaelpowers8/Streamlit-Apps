24f5f45d1add87f30b845aaf6b3c8162b7809d7a
michaelpowers8 <126989418+michaelpowers8@users.noreply.github.com>
2025-02-07 14:39:31 -0600

    Update Learning_Streamlit_Parts.py

--- Learning_Streamlit_Parts.py
+++ Learning_Streamlit_Parts.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from pandas import DataFrame
+from time import sleep
 import numpy.random as random_array
 
 st.header("Miscellaneous Stuff")
@@ -13,3 +13,23 @@ st.error("Something Failed... JK")
 st.dataframe(random_array.rand(3,2))
 
 st.text_input("Text Input")
+
+st.button("BUTTON")
+
+st.code("Code")
+
+st.checkbox("Checkbox")
+
+st.download_button("Download",random_array.rand(3,2),"Download_Data.csv")
+
+uploaded_file = st.file_uploader("Choose a file")
+
+st.html("<h1>Cheers HTML</h1>")
+
+progress_text:str = "Operation in progress. Please wait."
+my_bar = st.progress(0, text=progress_text)
+for percent_complete in range(100):
+    sleep(0.01)
+    my_bar.progress(percent_complete + 1, text=progress_text)
+sleep(1)
+my_bar.empty()
