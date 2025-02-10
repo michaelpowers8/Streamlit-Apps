5b99c31a6b257c467379516ed58f5339c8b53c73
michaelpowers8 <126989418+michaelpowers8@users.noreply.github.com>
2025-02-10 14:32:29 -0600

    Update Self_Made_Slots_Calculator.py

--- Self_Made_Slots_Calculator.py
+++ Self_Made_Slots_Calculator.py
@@ -111,12 +111,28 @@ def display_images(results:list[str]):
 # Ensure results persist in session state
 if "results" not in st.session_state:
     st.session_state.server_seed = generate_seed() 
+    st.session_state.server_seed_hashed = sha256_encrypt(st.session_state.server_seed)
     st.session_state.client_seed = generate_seed(20)
     st.session_state.nonce = 1
+    st.session_state.prizes = [
+                                    'cherry', 'lemon', 'bell', 
+                                    'clover', 'diamond', 'star', 
+                                    'caterpillar', 'butterfly', 'angel_butterfly'
+                                ]
+    st.session_state.images = [
+                                    Image.open('cherry.jpg'),Image.open('lemon.jpg'),Image.open('bell.jpg'),
+                                    Image.open('clover.jpg'),Image.open('diamond.jpg'),Image.open('star.jpg'),
+                                    Image.open('caterpillar.jpg'),Image.open('butterfly.jpg'),Image.open('angel_butterfly.jpg')
+                                ]
+    st.session_state.multipliers = [1.3, 2.25, 3.00, 5.00, 10.0, 25.0,  50.0, 125.0, 5000]
+    st.session_state.nonce = 0
+    st.session_state.balance = 10_000
+    st.session_state.bet_amount = 0
     st.session_state.results = seeds_to_results(
         st.session_state.server_seed, 
         st.session_state.client_seed, 
-        st.session_state.nonce
+        st.session_state.nonce,
+        
     )
 
 def update_screen():
