14fb185dda4abccc1ce792620175011a96bbf928
michaelpowers8 <126989418+michaelpowers8@users.noreply.github.com>
2025-02-10 14:55:32 -0600

    Update Self_Made_Slots_Calculator.py

--- Self_Made_Slots_Calculator.py
+++ Self_Made_Slots_Calculator.py
@@ -136,6 +136,7 @@ if "results" not in st.session_state:
     st.session_state.nonce += 1
 
 def update_screen():
+    st.header("Fluttering Riches - Slot Machine")
     st.session_state.bet_amount = st.number_input("Bet Amount:", min_value=0, max_value=st.session_state.balance, key=generate_seed(1_000))
     display_seed_information()
     display_images(st.session_state.results)  # Use session_state.results
