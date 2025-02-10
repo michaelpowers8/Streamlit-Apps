74e7d54142fd484f4b765a775fe14f04119dc7fc
michaelpowers8 <126989418+michaelpowers8@users.noreply.github.com>
2025-02-10 14:13:26 -0600

    Update Self_Made_Slots_Calculator.py

--- Self_Made_Slots_Calculator.py
+++ Self_Made_Slots_Calculator.py
@@ -126,8 +126,8 @@ if 'server_seed' not in st.session_state:
     st.session_state.multipliers = [1.3, 2.25, 3.00, 5.00, 10.0, 25.0,  50.0, 125.0, 5000]
     st.session_state.nonce = 0
     st.session_state.balance = 10_000
-    st.session_state.bet_amount = 200
-    st.number_input("")
+    st.session_state.bet_amount = 0
+    st.session_state.bet_amount = st.number_input("Bet Amount:")
     results:list[list[str]] = seeds_to_results(st.session_state.server_seed, st.session_state.client_seed, st.session_state.nonce, st.session_state.prizes)
     st.session_state.nonce += 1
     display_seed_information()
@@ -140,6 +140,7 @@ if st.button("Spin"):
         st.session_state.balance -= st.session_state.bet_amount
         results = seeds_to_results(st.session_state.server_seed, st.session_state.client_seed, st.session_state.nonce, st.session_state.prizes)
         st.session_state.nonce += 1
+        st.session_state.bet_amount = st.number_input("Bet Amount:")
         display_seed_information()
         display_images(results)
         wins = check_for_wins(results)
