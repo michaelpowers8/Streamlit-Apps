0122f72c6124832fe746ffc1e79d5cd4923620e0
michaelpowers8 <126989418+michaelpowers8@users.noreply.github.com>
2025-02-10 14:56:05 -0600

    Update Self_Made_Slots_Calculator.py

--- Self_Made_Slots_Calculator.py
+++ Self_Made_Slots_Calculator.py
@@ -108,6 +108,7 @@ def display_images(results:list[str]):
         st.image(st.session_state.images[st.session_state.prizes.index(results[1][2])])
         st.image(st.session_state.images[st.session_state.prizes.index(results[2][2])])
 
+st.header("Fluttering Riches - Slot Machine")
 # Ensure results persist in session state
 if "results" not in st.session_state:
     st.session_state.server_seed = generate_seed() 
@@ -136,7 +137,6 @@ if "results" not in st.session_state:
     st.session_state.nonce += 1
 
 def update_screen():
-    st.header("Fluttering Riches - Slot Machine")
     st.session_state.bet_amount = st.number_input("Bet Amount:", min_value=0, max_value=st.session_state.balance, key=generate_seed(1_000))
     display_seed_information()
     display_images(st.session_state.results)  # Use session_state.results
